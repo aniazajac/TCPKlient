5519e345321b31729bcc63063b2f78c627eaeafa
student <student@PC-218>
2017-05-24 16:13:27 +0200

    Zakończony interfejs

--- TCPKlient/Form1.Designer.cs
+++ TCPKlient/Form1.Designer.cs
@@ -28,12 +28,74 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.adres = new System.Windows.Forms.TextBox();
+            this.my_port = new System.Windows.Forms.NumericUpDown();
+            this.info_o_polaczeniu = new System.Windows.Forms.ListBox();
+            this.polacz = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.my_port)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // adres
+            // 
+            this.adres.Location = new System.Drawing.Point(1, 3);
+            this.adres.Name = "adres";
+            this.adres.Size = new System.Drawing.Size(306, 20);
+            this.adres.TabIndex = 0;
+            this.adres.Text = "Adres";
+            // 
+            // my_port
+            // 
+            this.my_port.Location = new System.Drawing.Point(313, 4);
+            this.my_port.Maximum = new decimal(new int[] {
+            65535,
+            0,
+            0,
+            0});
+            this.my_port.Name = "my_port";
+            this.my_port.Size = new System.Drawing.Size(376, 20);
+            this.my_port.TabIndex = 1;
+            // 
+            // info_o_polaczeniu
+            // 
+            this.info_o_polaczeniu.FormattingEnabled = true;
+            this.info_o_polaczeniu.Location = new System.Drawing.Point(1, 29);
+            this.info_o_polaczeniu.Name = "info_o_polaczeniu";
+            this.info_o_polaczeniu.Size = new System.Drawing.Size(688, 303);
+            this.info_o_polaczeniu.TabIndex = 2;
+            // 
+            // polacz
+            // 
+            this.polacz.Location = new System.Drawing.Point(270, 349);
+            this.polacz.Name = "polacz";
+            this.polacz.Size = new System.Drawing.Size(119, 46);
+            this.polacz.TabIndex = 3;
+            this.polacz.Text = "polacz";
+            this.polacz.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(701, 423);
+            this.Controls.Add(this.polacz);
+            this.Controls.Add(this.info_o_polaczeniu);
+            this.Controls.Add(this.my_port);
+            this.Controls.Add(this.adres);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.my_port)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox adres;
+        private System.Windows.Forms.NumericUpDown my_port;
+        private System.Windows.Forms.ListBox info_o_polaczeniu;
+        private System.Windows.Forms.Button polacz;
     }
 }
 
